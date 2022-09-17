034e0ca6d5225f2f2e59a9662babe656746c3578
Petros-Emmanouilidis <pemmanou@andrew.cmu.edu>
2022-09-18 00:06:24 +0300

    something

--- frontend/src/PopUp.js
+++ frontend/src/PopUp.js
@@ -1,8 +1,19 @@
-export default function PopUp (location) {
-    console.log("Inside popup", location)
-    return (
-        <h1>
-            {location.src}
-        </h1>
-    )
-}
+// export default function PopUp (location) {
+//     console.log("Inside popup", location)
+//     return (
+//         <h1>
+//             {location.src}
+//         </h1>
+//     )
+// }
+
+import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+export default function PopUp (location){
+  <Popup trigger={<button> Trigger</button>} position="right center">
+    <div>well this works!</div>
+  </Popup>
+};
+
